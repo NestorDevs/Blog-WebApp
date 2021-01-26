4f36be0418fd453c85e64a08fa63513804612cc8
NestorDevs <neivertjosias@gmail.com>
2021-01-26 15:08:05 -0400

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { AuthenticationService } from './../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.authService
+      .login('test1@test.com', '123456')
+      .subscribe((data) => console.log('SUCCESS'));
+  }
+}
